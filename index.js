f7547c1021b4625c68023b2a59330e3798410691
Ian Sebleius <sebeliusancira@gmail.com>
2023-06-20 16:53:06 -0700

    implement fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const {fetchMyIP, fetchCoordsByIP} = require('./iss');
+const {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes} = require('./iss');
 
-fetchMyIP((error, ip) => {
+const exampleCoords = {latitude: '49.27670', longitude: '-123.13000'};
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
-    console.log('It didn"t work ', error);
+    console.log('It didn\'t work!', error);
     return;
   }
-  console.log('It worked, Returned IP: ', ip);
-  // fetchCoordsByIP(ip);
+
+  console.log('It worked! Returned flyover times: ', passTimes);
 });
 
-fetchCoordsByIP('24.68.230.70', (error, geo) => {
-  if (error) {
-    console.log('there was an error: ', error);
-  }
-  console.log('It worked, Returned Geo Coordinates: ', geo);
-});
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log('It didn"t work ', error);
+//     return;
+//   }
+//   console.log('It worked, Returned IP: ', ip);
+//   // fetchCoordsByIP(ip);
+// });
+
+// fetchCoordsByIP('24.68.230.70', (error, geo) => {
+//   if (error) {
+//     console.log('there was an error: ', error);
+//   }
+//   console.log('It worked, Returned Geo Coordinates: ', geo);
+// });
