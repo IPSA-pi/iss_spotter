494efdf31d1c2798e5371396fa4a99d4de9d831f
Ian Sebleius <sebeliusancira@gmail.com>
2023-06-20 19:18:48 -0700

    implemented nextISSTimesForMyLocation to call all functions

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-const {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes} = require('./iss');
+const {nextISSTimesForMyLocation} = require('./iss');
 
 const exampleCoords = {latitude: '49.27670', longitude: '-123.13000'};
 
-fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
-  if (error) {
-    console.log('It didn\'t work!', error);
-    return;
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
+};
 
-  console.log('It worked! Returned flyover times: ', passTimes);
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  printPassTimes(passTimes);
 });
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log('It didn"t work ', error);
-//     return;
-//   }
-//   console.log('It worked, Returned IP: ', ip);
-//   // fetchCoordsByIP(ip);
-// });
-
-// fetchCoordsByIP('24.68.230.70', (error, geo) => {
-//   if (error) {
-//     console.log('there was an error: ', error);
-//   }
-//   console.log('It worked, Returned Geo Coordinates: ', geo);
-// });
